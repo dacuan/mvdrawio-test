193dd5a5dc993196583d2a449e8b25c73c3a4139
Victor Grischenko <dacuan@yandex.ru>
2022-03-22 21:55:01 +0300

    Create mvm-abb.js

---
+++ mvm-abb.js
@@ -0,0 +1,23 @@
+mvm_abb = [
+  {
+    "id": "1",
+    "title": "GIS",
+    "link": "https://wiki.mvideo.ru/pages/viewpage.action?pageId=228162480",
+    "owner": "Иван Петров",
+    "prodDate": "24.03.2022"
+  },
+  {
+    "id": "2",
+    "title": "Speech2Text",
+    "link": "https://wiki.mvideo.ru/pages/viewpage.action?pageId=228162480",
+    "owner": "Никита Кожемяка",
+    "prodDate": "01.12.2021"
+  },
+  {
+    "id": "3",
+    "title": "Агрегатор чатов",
+    "link": "https://wiki.mvideo.ru/pages/viewpage.action?pageId=228162480",
+    "owner": "Алла Дроздова",
+    "prodDate": "12.05.2020"
+  }
+];
