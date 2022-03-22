737d60bc9ef90b24a9ef6c1120498db328836eaa
Victor Grischenko <dacuan@yandex.ru>
2022-03-22 19:58:30 +0300

    First test

---
+++ mvmlib.js
@@ -0,0 +1,22 @@
+/**
+ * External plugin to be added to draw.io
+ */
+
+ Draw.loadPlugin(function(ui) {
+
+    if (ui.editor.isChromelessView()) {
+        return;
+    }
+	console.log('123456', {1:'Hello', 2:'World'});
+    // Add sidebar Stencil
+    ui.sidebar.addStencilPalette('flowchart', 'M.Video ABB', 'ttps://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Security-Icons/stencil-security.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
+
+    // Collapses default sidebar entry and inserts this before
+    var c = ui.sidebar.container;
+    c.firstChild.click();
+    c.insertBefore(c.lastChild, c.firstChild);
+    c.insertBefore(c.lastChild, c.firstChild);
+    c.firstChild.click();
+
+
+});
